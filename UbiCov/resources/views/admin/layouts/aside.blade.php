c9578223ee19bca3c4611645b7d18f7f3afba8d7
Aneth Guadalupe Ochoa Lozano <71891002+AnethOchoa@users.noreply.github.com>
2021-06-08 11:27:56 -0600

    Aside

--- UbiCov/resources/views/admin/layouts/aside.blade.php
+++ UbiCov/resources/views/admin/layouts/aside.blade.php
@@ -2,8 +2,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{ asset('/public/img/logo1.png') }}" alt="" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">UbiCov</span>
+      <img src="dashboard/dist/img/Slogan2.png" alt="" class="brand-image " style="opacity: .8">
+      <span class="brand-text font-weight-light">Bienvenido</span>
     </a>
 
     <!-- Sidebar -->
@@ -11,7 +11,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="dashboard/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -21,7 +21,7 @@
       <!-- SidebarSearch Form -->
       <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Buscar" aria-label="Search">
           <div class="input-group-append">
             <button class="btn btn-sidebar">
               <i class="fas fa-search fa-fw"></i>
@@ -45,25 +45,25 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
+                <a href="usuarios" class="nav-link active">
                   <i class="far fa-user nav-icon"></i>
-                  <p>Usuarios registrados</p>
+                  <p>Negocios registrados</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
+                <a href="mapa" class="nav-link">
                   <i class="far fa-map nav-icon"></i>
                   <p>Mapa</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
+                <a href="graficos" class="nav-link">
                   <i class="far fa-heart nav-icon"></i>
                   <p>Gráficos</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
+                <a href="semaforizacion" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Semaforización</p>
                 </a>
