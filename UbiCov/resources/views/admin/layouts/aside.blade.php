e5dd5b1ce1347eee1f482801b7d54ef256ef79a9
Aneth Guadalupe Ochoa Lozano <71891002+AnethOchoa@users.noreply.github.com>
2021-06-15 13:55:01 -0600

    Aneth

--- UbiCov/resources/views/admin/layouts/aside.blade.php
+++ UbiCov/resources/views/admin/layouts/aside.blade.php
@@ -1,60 +1,44 @@
-<div class="sidebar" data-color="purple" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
+<div class="sidebar" data-color="purple" data-background-color="white" data-image="../public/img/login.png">
       <!--
         Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
         Tip 2: you can also add an image using data-image tag
     -->
       <div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-normal">
-          Creative Tim
+         <!--
+      poner aqui el nombre del usuario y nivel
+    -->
         </a></div>
       <div class="sidebar-wrapper">
         <ul class="nav">
           <li class="nav-item active  ">
-            <a class="nav-link" href="./dashboard.html">
+            <a class="nav-link" href="/admin">
               <i class="material-icons">dashboard</i>
-              <p>Dashboard</p>
+              <p>Panel de administración</p>
             </a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link" href="./user.html">
+            <a class="nav-link" href="/admin/users">
               <i class="material-icons">person</i>
-              <p>User Profile</p>
+              <p>Usuarios</p>
             </a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link" href="./tables.html">
-              <i class="material-icons">content_paste</i>
-              <p>Table List</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="./typography.html">
-              <i class="material-icons">library_books</i>
-              <p>Typography</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="./icons.html">
+            <a class="nav-link" href="/admin/semaforizacion">
               <i class="material-icons">bubble_chart</i>
-              <p>Icons</p>
+              <p>Semaforización</p>
             </a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link" href="./map.html">
+            <a class="nav-link" href="/admin/mapa">
               <i class="material-icons">location_ons</i>
-              <p>Maps</p>
+              <p>Mapa</p>
             </a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link" href="./notifications.html">
-              <i class="material-icons">notifications</i>
-              <p>Notifications</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="./rtl.html">
+            <a class="nav-link" href="/admin/graficos">
               <i class="material-icons">language</i>
-              <p>RTL Support</p>
+              <p>Gráficos</p>
             </a>
           </li>
           <li class="nav-item active-pro ">
